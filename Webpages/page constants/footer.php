d45e2aeea039f2ba611b9e67d41c89453f1cc5ec
Dilim <obi.sam.ibo@gmail.com>
2018-12-05 23:21:39 -0500

    formatting the index page

---
+++ Webpages/page constants/footer.php
@@ -0,0 +1,80 @@
+
+<html>
+
+  <!--Footer-->
+
+  <footer class="footer theme-bg">
+    <section class="footer-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-lg-4 sm-m-15px-tb md-m-30px-b">
+            <h4 class="font-alt">About Us</h4>
+            <p class="footer-text">A modular trading application to help you make more money. We gather data from the OANDA api to show you market data and to guide you on data driven trading decisions</p>
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a class="google" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fab fa-linkedin-in"></i></a></li>
+            </ul>
+          </div> <!-- col -->
+
+          <div class="col-6 col-md-4 col-lg-2 sm-m-15px-tb">
+            <h4 class="font-alt">Product</h4>
+            <ul class="fot-link">
+              <li><a href="#">Features</a></li>
+              <li><a href="#">Users Program</a></li>
+              <li><a href="#">Locations</a></li>
+            </ul>
+          </div> <!-- col -->
+
+          <div class="col-6 col-md-4 col-lg-2 sm-m-15px-tb">
+            <h4 class="font-alt">Company</h4>
+            <ul class="fot-link">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Policy</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+
+          <div class="col-md-4 col-lg-4 sm-m-15px-tb">
+            <h4 class="font-alt">Subscribe</h4>
+            <p>Subscribe to our email list for general news and updates</p>
+            <div class="subscribe-box m-20px-t">
+              <input placeholder="Enter Email Id" class="form-control" type="text" name="demo">
+              <button class="btn btn-theme"><i class="ti-arrow-right"></i></button>
+            </div>
+          </div> <!-- col -->
+
+        </div>
+
+        <div class="footer-copy">
+          <div class="row">
+            <div class="col-12">
+                <p>2018 AuguryX. All Rights Reserved developed by AuguryX </p>
+            </div><!-- col -->
+          </div> <!-- row -->
+        </div> <!-- footer-copy -->
+
+      </div> <!-- container -->
+    </section>
+  </footer>
+  <!-- / -->
+
+
+  <!-- Jquery -->
+  <script src="static/js/jquery-3.2.1.min.js"></script>
+  <script src="static/js/jquery-migrate-3.0.0.min.js"></script>
+  <!-- End -->
+
+  <!-- Plugin JS -->
+  <script src="static/plugin/appear/jquery.appear.js"></script><!--appear-->
+  <script src="static/plugin/bootstrap/js/bootstrap.js"></script><!--bootstrap-->
+
+  <script src="static/plugin/particles/particles.min.js"></script><!--bootstrap-->
+  <script src="static/plugin/particles/particles-app.js"></script><!--bootstrap-->
+  <!-- End -->
+
+  <!-- Custom -->
+  <script src="static/js/custom.js"></script>
+  <!-- End -->
